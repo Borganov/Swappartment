adbcefd31fc61c62fbac7fe9c013568d6620cbfd
Borganov <berclazalexandre@gmail.com>
2019-07-11 20:03:32 +0200

    choose img work

--- app/src/main/java/ch/hevs/swap/ui/apartment/addApartment.java
+++ app/src/main/java/ch/hevs/swap/ui/apartment/addApartment.java
@@ -1,21 +1,77 @@
 package ch.hevs.swap.ui.apartment;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+
+import java.io.IOException;
 
 import ch.hevs.swap.R;
 
 public class addApartment extends AppCompatActivity {
 
+    private Button btnChoose, btnUpload;
+    private ImageView imageView;
+
+    private Uri filepath;
+
+    private final int PICK_IMAGE_REQUEST = 71;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_apartment);
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference imagesRef = storageRef.child('img')
+
+        //Init view
+        btnChoose = (Button)findViewById(R.id.btnChoose);
+        btnUpload = (Button)findViewById(R.id.btnUpload);
+        imageView= (ImageView)findViewById(R.id.imageView);
+
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null ){
+            filepath = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filepath);
+                imageView.setImageBitmap(bitmap);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+
+
     }
 }
