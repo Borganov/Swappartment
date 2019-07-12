64741819dda17fa7cb9761bfec06aee834c5bee4
Gab <thinkys@live.com>
2019-07-12 09:46:41 +0200

    Add Locality and LocalityEntity

--- app/src/main/java/ch/hevs/swap/data/entity/LocalityEntity.java
+++ app/src/main/java/ch/hevs/swap/data/entity/LocalityEntity.java
@@ -13,34 +13,50 @@ public class LocalityEntity implements Locality {
     @ColumnInfo(name="idLocality")
     private Long idLocality;
 
-    @ColumnInfo(name="name")
-    private String name;
+    @ColumnInfo(name="nameLocality")
+    private String nameLocality;
 
-    @ColumnInfo(name="zipcode")
-    private String zipcode;
+    @ColumnInfo(name="npa")
+    private Integer npa;
 
+    @ColumnInfo(name="canton")
+    private String canton;
+
+    @ColumnInfo(name="nrLocality")
+    private Integer nrLocality;
 
     public Long getIdLocality() {
         return idLocality;
     }
 
     public String getName() {
-        return name;
+        return nameLocality;
     }
 
-    public String getZipCode() {
-        return zipcode;
-    }
+    public Integer getNpa() { return npa; }
+
+    public String getCanton() { return canton; }
+
+    public Integer getNrLocality() { return nrLocality;}
+
 
     public void setIdLocality(Long idLocality) {
         this.idLocality = idLocality;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setNameLocality(String nameLocality) {
+        this.nameLocality = nameLocality;
     }
 
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    public void setNpa(Integer npa) {
+        this.npa = npa;
+    }
+
+    public void setCanton(String canton) {
+        this.canton = canton;
+    }
+
+    public void setNrLocality(Integer nrLocality) {
+        this.nrLocality = nrLocality;
     }
 }
--- app/src/main/java/ch/hevs/swap/data/model/Locality.java
+++ app/src/main/java/ch/hevs/swap/data/model/Locality.java
@@ -3,5 +3,13 @@ package ch.hevs.swap.data.model;
 public interface Locality  {
     Long getIdLocality();
     String getName();
-    String getZipCode();
+    Integer getNpa();
+    String getCanton();
+    Integer getNrLocality();
+
+    void setIdLocality(Long idLocality);
+    void setNameLocality(String nameLocality);
+    void setNpa(Integer npa);
+    void setCanton(String canton);
+    void setNrLocality(Integer nrLocality);
 }
