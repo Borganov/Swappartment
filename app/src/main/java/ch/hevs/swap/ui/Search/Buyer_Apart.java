b522d4267937b7d6ab9e705261258d4af6c8190c
VOUGY <guillaume.vouilloz@outlook.com>
2019-07-12 14:45:41 +0200

    Feat : Create Activity

---
+++ app/src/main/java/ch/hevs/swap/ui/Search/Buyer_Apart.java
@@ -0,0 +1,15 @@
+package ch.hevs.swap.ui.Search;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.hevs.swap.R;
+
+public class Buyer_Apart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer__apart);
+    }
+}
