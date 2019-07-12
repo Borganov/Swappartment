f6b2667187e7b00702ef5cd10c3a9a7ed87096ed
Gab <thinkys@live.com>
2019-07-12 11:47:09 +0200

    Push models folder with errors + layout newAppart

---
+++ app/src/main/java/ch/hevs/swap/data/models/Appart.java
@@ -0,0 +1,29 @@
+package ch.hevs.swap.data.models;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class Appart {
+
+    public String type;
+    public Integer nbRooms;
+    public Integer price;
+    public String addressStreet;
+    public Integer addressNo;
+    public String localityId;
+    public String comment;
+
+    public Appart() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public Appart(String type, Integer nbRooms, Integer price, String addressStreet, Integer addressNo, String localityId, String comment) {
+        this.type = type;
+        this.nbRooms = nbRooms;
+        this.price = price;
+        this.addressStreet = addressStreet;
+        this.addressNo = addressNo;
+        this.localityId = localityId;
+        this.comment = comment;
+    }
+}
