e0572df67e4b414dfd2f0809418e9be3d920d23e
Gab <thinkys@live.com>
2019-07-12 18:43:42 +0200

    display locality autoCompleteSearchField working

--- app/src/main/java/ch/hevs/swap/ui/search/Buyer_Appart.java
+++ app/src/main/java/ch/hevs/swap/ui/search/Buyer_Appart.java
@@ -1,24 +1,21 @@
 package ch.hevs.swap.ui.search;
 
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
@@ -30,17 +27,30 @@ import ch.hevs.swap.data.models.Appart;
 
 public class Buyer_Appart extends AppCompatActivity {
 
-    private EditText mSearchField;
-
     private Button mBtnLaunchSearch;
+
+    List<String> countries;
     List<Appart> apparts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buyer_appart);
 
-        mSearchField = findViewById(R.id.searchField);
+//        mSearchField = findViewById(R.id.searchField);
         mBtnLaunchSearch = findViewById(R.id.btnLaunchSearch);
+        countries = new ArrayList<>();
+
+        ListCountries();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, countries);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.autoComplete_Locality);
+        textView.setDropDownVerticalOffset(2);
+        textView.setAdapter(adapter);
+
+
         apparts = new ArrayList<>();
 
         mBtnLaunchSearch.setOnClickListener(new View.OnClickListener() {
@@ -49,9 +59,7 @@ public class Buyer_Appart extends AppCompatActivity {
                 search();
             }
         });
-
-
-    }
+    };
 
     public void search() {
 
@@ -59,18 +67,27 @@ public class Buyer_Appart extends AppCompatActivity {
         ConstraintSet set = new ConstraintSet();
 
         String[] results = new String[10];
-         ListAppart();
-     //   mBtnLaunchSearch.setText(apparts.get(1).addressStreet);
+        ListAppart();
+        //   mBtnLaunchSearch.setText(apparts.get(1).addressStreet);
 
+        for (int i = 0; i < results.length; i++) {
+            TextView view = new TextView(this);
+            view.setId(View.generateViewId());
+            view.setText(results[i]);
+            layout.addView(view, i);
+            set.clone(layout);
+            set.connect(view.getId(), ConstraintSet.TOP, layout.getId(), ConstraintSet.TOP, 60 + (i * 70));
+            set.applyTo(layout);
+        }
 
+    };
 
-    }
 
     private void ListAppart() {
         FirebaseDatabase mDatabase;
         DatabaseReference mDataBaseRef = FirebaseDatabase.getInstance().getReference();
         mDatabase = FirebaseDatabase.getInstance();
-       //mDataBaseRef = mDatabase.getReference().child("swap-appartements").child("appart");
+        //mDataBaseRef = mDatabase.getReference().child("swap-appartements").child("appart");
 
 
         Query query = mDataBaseRef.child("appart");
@@ -80,7 +97,7 @@ public class Buyer_Appart extends AppCompatActivity {
                 if (dataSnapshot.exists()) {
                     // dataSnapshot is the "issue" node with all children with id 0
                     for (DataSnapshot issue : dataSnapshot.getChildren()) {
-                        apparts.add(new Appart((String)issue.child("type").getValue(), (long)issue.child("nbRooms").getValue(),(long)issue.child("price").getValue(),(String)issue.child("addressStreet").getValue(),(String)issue.child("userId").getValue()));
+                        apparts.add(new Appart((String) issue.child("type").getValue(), (long) issue.child("nbRooms").getValue(), (long) issue.child("price").getValue(), (String) issue.child("addressStreet").getValue(), (String) issue.child("userId").getValue()));
 
                         //String type, int nbRooms, int price, String adressStreet, String userId
                     }
@@ -93,4 +110,33 @@ public class Buyer_Appart extends AppCompatActivity {
             }
         });
     }
+
+    private void ListCountries() {
+    FirebaseDatabase mDatabase;
+    DatabaseReference mDataBaseRef = FirebaseDatabase.getInstance().getReference();
+    mDatabase = FirebaseDatabase.getInstance();
+
+    Query query = mDataBaseRef.child("Localities");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String rst;
+                    // dataSnapshot is the "issue" node with all children with id 0
+                    for (DataSnapshot issue : dataSnapshot.getChildren()) {
+                        rst = (String) issue.child("nameLocality").getValue() + " (" + (String) issue.child("npa").getValue() + ")";
+                        System.out.println(rst);
+                        countries.add(rst);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
 }
