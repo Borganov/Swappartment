b74460b1be33c8b68a6aa65fe51008cde7aef314
Gab <thinkys@live.com>
2019-08-30 17:04:11 +0200

    Validate transfer of apartment ids arraylist from search to result

--- app/src/main/java/ch/hevs/swap/ui/homepage/HomePage.java
+++ app/src/main/java/ch/hevs/swap/ui/homepage/HomePage.java
@@ -16,13 +16,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import ch.hevs.swap.R;
 import ch.hevs.swap.ui.login.LoginEditProfil;
 import ch.hevs.swap.ui.login.LoginForgetPassword;
-import ch.hevs.swap.ui.newAppart.NewAppartActivity;
-import ch.hevs.swap.ui.search.Buyer_Appart;
+import ch.hevs.swap.ui.search.SearchApart;
 
 public class HomePage extends AppCompatActivity implements View.OnClickListener {
 
@@ -52,7 +50,7 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener 
 
         if(!user.getEmail().isEmpty())
         {
-            Intent intent = new Intent(HomePage.this, Buyer_Appart.class);
+            Intent intent = new Intent(HomePage.this, SearchApart.class);
             intent.setFlags(
                     Intent.FLAG_ACTIVITY_NO_ANIMATION |
                             Intent.FLAG_ACTIVITY_NO_HISTORY
@@ -85,7 +83,7 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener 
         } else if (i == R.id.HomePage_Login)
         {
             signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
-            Intent intent = new Intent(HomePage.this, Buyer_Appart.class);
+            Intent intent = new Intent(HomePage.this, SearchApart.class);
             intent.setFlags(
                     Intent.FLAG_ACTIVITY_NO_ANIMATION |
                             Intent.FLAG_ACTIVITY_NO_HISTORY
