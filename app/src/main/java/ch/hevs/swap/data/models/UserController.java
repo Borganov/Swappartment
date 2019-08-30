15e7891d693260ed45f39d108693d72703ec696b
VOUGY <guillaume.vouilloz@outlook.com>
2019-08-30 15:41:24 +0200

    Update UserController.java

--- app/src/main/java/ch/hevs/swap/data/models/UserController.java
+++ app/src/main/java/ch/hevs/swap/data/models/UserController.java
@@ -1,5 +1,19 @@
 package ch.hevs.swap.data.models;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class UserController {
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
+    public void AddApartViewed(String uidApp, boolean like)
+    {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase.child("users/"+ mAuth.getCurrentUser().getUid()+"/apartViewed/" + uidApp).setValue(like);
+        // usercon.AddApartViewed(appartQueue.getFirst().getInfo().getValeur(),true);
+    }
 
 }
